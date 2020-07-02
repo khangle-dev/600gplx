839f7f86aa23d53abf8bc3be58521270ffdcf341
khangle-dev <66984755+khangle-dev@users.noreply.github.com>
2020-07-02 19:45:49 +0700

    Update list.js

--- controllers/list.js
+++ controllers/list.js
@@ -4,6 +4,23 @@ app.controller("listCtrl", function ($scope) {
     $scope.countSai = 0;
     $scope.list = [];
 
+    fullQuestions.forEach(function(question){
+        var danger_css =question.required > 0 ? "danger" : ""
+        var css_class = "" + danger_css;
+
+        if (!hasAnswered(license.code, question.index)) {
+            $scope.countChuaLam ++;
+        } else if (isAnsweredWrong(license.code, question.index)) {
+            $scope.countSai ++;
+            css_class = "wrong" + danger_css;
+        } else {
+            $scope.countDung ++;
+            css_class = "correct" + danger_css;
+        }
+
+        $scope.list.push({index: question.index, css_class: css_class});
+    })
+        /*
     for (var i = 0; i < fullQuestions.length; i++) {
         var danger_css = fullQuestions[i].required > 0 ? "danger" : ""
         var css_class = "" + danger_css;
@@ -19,5 +36,5 @@ app.controller("listCtrl", function ($scope) {
         }
 
         $scope.list.push({index: i, css_class: css_class});
-    }
+    }*/
 });
